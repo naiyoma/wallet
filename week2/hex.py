e3c43463bc5c226c18d6037f509b9bd24062dc04
naiyoma <lankas.aurelia@gmail.com>
2024-02-21 02:36:16 +0300

    script update

--- week2/hex.py
+++ week2/hex.py
@@ -1,42 +1,113 @@
-
-from bitcoinlib.transactions import Transaction
 from pprint import pprint
 
+class Transaction:
+    def __init__(self, version, inputs, outputs, locktime):
+        self.version = version
+        self.inputs = inputs
+        self.outputs = outputs
+        self.locktime = locktime
 
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig, sequence):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        self.script_sig = script_sig
+        self.sequence = sequence
 
-def parse_transactions(hex):
-    """
-    Parse a transactions.
-    """
-    transaction = Transaction.parse_hex(hex)
-    inputs = [
-        {
-            "txid": input.prev_txid.hex(),
-            "vout": input.output_n_int,
-            "signatures": [signature.hex() for signature in input.signatures],
-            "witness_type": input.witness_type,
-            "witness": [witness.hex() for witness in input.witnesses]
-        }
-        for input in  transaction.inputs
-    ]
-    outputs = [
-        {
-            "address": output.address,
-            "value":output.value,
-            "output_n":output.output_n,
-            "hex":output.script.serialize().hex()
-        }
-        for output in transaction.outputs
-    ]
-    pprint(
-        {
-        "txid": transaction.txid,
-        "confirmations": transaction.confirmations,
-        "version": transaction.version.hex(),
-        "input": inputs,
-        "outputs": outputs,
-        "locktime": transaction.locktime
-        }
-    )
+class TxOut:
+    def __init__(self, value, script_pubkey):
+        self.value = value
+        self.script_pubkey = script_pubkey
+
+def decode_transaction(hex_string):
+    # Perform hex decoding
+    raw_bytes = bytes.fromhex(hex_string)
+    # Parse transaction fields
+    version = raw_bytes[0:4]
+    num_inputs = int.from_bytes(raw_bytes[6:7], byteorder='little')  # Corrected indexing and conversion to integer
+    inputs = []
+
+    # Parse inputs
+    offset = 7
+    for _ in range(num_inputs):
+        prev_tx = raw_bytes[offset:offset+32]
+        prev_index = raw_bytes[offset+32:offset+36]
+        script_sig_length = raw_bytes[offset+36]
+        script_sig = raw_bytes[offset+37:offset+37+script_sig_length]
+        sequence = raw_bytes[offset+37+script_sig_length:offset+37+script_sig_length+4]
+        inputs.append(TxIn(prev_tx, prev_index, script_sig, sequence))
+        offset += 37 + script_sig_length + 4
+
+    num_outputs = raw_bytes[offset]
+    outputs = []
+    offset += 1
+    for _ in range(num_outputs):
+        value = int.from_bytes(raw_bytes[offset:offset+8], byteorder='little')
+        script_pubkey_length = raw_bytes[offset+8]
+        script_pubkey = raw_bytes[offset+9:offset+9+script_pubkey_length]
+        outputs.append(TxOut(value, script_pubkey))
+        offset += 9 + script_pubkey_length
+    # # import pdb; pdb.set_trace()
+    # print("Output Count:\n")
+    # print("Hex:", format(num_outputs, '02x'))
+    # for i, (value, script_pubkey) in enumerate(outputs, start=1):
+    #     print(f"\nOutput {i}:\n")
+    #     print(f"Value: {value:016x} (In Satoshis, so {value} Satoshis)")
+    #     print(f"Script Length: {format(script_pubkey_length, '02x')}")
+    #     print("Script Pubkey:", script_pubkey.hex())
+
     
-parse_transactions("020000000001010ccc140e766b5dbc884ea2d780c5e91e4eb77597ae64288a42575228b79e234900000000000000000002bd37060000000000225120245091249f4f29d30820e5f36e1e5d477dc3386144220bd6f35839e94de4b9cae81c00000000000016001416d31d7632aa17b3b316b813c0a3177f5b6150200140838a1f0f1ee607b54abf0a3f55792f6f8d09c3eb7a9fa46cd4976f2137ca2e3f4a901e314e1b827c3332d7e1865ffe1d7ff5f5d7576a9000f354487a09de44cd00000000")
+    transaction = Transaction(version, inputs, outputs,1)
+    
+    return transaction
+
+def deserialize_transaction(transaction):
+   
+    deserialized_inputs = []
+    deserialized_outputs = []
+
+    for input in transaction.inputs:
+        prev_tx_hex = input.prev_tx.hex()
+        # Convert the hex string to bytes
+        bytes_data = bytes.fromhex(prev_tx_hex)
+        # Reverse the bytes
+        reversed_bytes = bytes_data[::-1]
+        # Convert the reversed bytes back to hex string
+        reversed_hex = reversed_bytes.hex()
+        vout_int = int.from_bytes(input.prev_index, byteorder='little')
+
+        deserialized_input = {
+            "prev_tx": reversed_hex,
+            "vout": vout_int,
+            "scriptSig": {
+                "hex": input.script_sig.hex()
+            },
+            "sequence": int(input.sequence[::-1].hex(), 16)
+        }
+        deserialized_inputs.append(deserialized_input)
+    # Deserialize outputs
+    for output in transaction.outputs:
+        # import pdb; pdb.set_trace()
+        value = output.value 
+        script_pubkey = output.script_pubkey.hex()  
+        deserialized_output = {
+            "value": value,
+            "scriptPubKey": {
+                "hex": script_pubkey
+            }
+        }
+        deserialized_outputs.append(deserialized_output)
+        
+    deserialized_transaction = {
+        "version": int.from_bytes(transaction.version, byteorder='little'),
+        "inputs": deserialized_inputs,
+        "outputs": deserialized_outputs
+    }
+
+    return deserialized_transaction
+
+# Example usage
+hex_transaction = "02000000000101c2b36b67a8e447765649e771bcb0450cfd06893025d80bac7a13ec3af7c4c77c0100000017160014fdde9936089bd0b1326fa954d0cdd8c831287cc4fdffffff020000000000000000066a04000102037fab402500000000225120f7ecd79ad135565bf182356a5aaa0b5fa814e9bfc32a931cb65825237f4eb87202473044022031db589b23dea0cf97287745f4cb697314de5a13cc6768dcfab7da4828dee52d022053571793abb91ba5d8c88a90d142854ce15eab5b6d980ed53f4884e62b86beef012102dfbd6e82b24409bb7bb94576bdfdf2ef22cc6f781efc337add8d2b05becca71b00000000"
+transaction = decode_transaction(hex_transaction)
+deserialized_transaction = deserialize_transaction(transaction)
+pprint(deserialized_transaction)
